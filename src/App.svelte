39db52d8b1e766a2fe2ed81e06e58f7134e0f3da
Max Dayton <daytonmaximus@gmail.com>
2022-08-01 23:52:19 -0600

    whatever

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,40 @@
 <script lang="ts">
+  const onClick = () => {
+    console.log("clicked");
+  };
 </script>
 
 <main>
 
   <h1>Clink</h1>
 
-  <table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Value</th>
-        <th>Edit</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>
-          <p>Income</p>
-        </td>
-        <td>
-          <p>$5943</p>
-        </td>
-        <td>
-          <button>✏️</button>
-        </td>
-        <td>
-          <button>🗑</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Value</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <p>Main</p>
+          </td>
+          <td>
+            <p>$5943</p>
+          </td>
+          <td>
+            <button on:click={onClick}>✏️</button>
+          </td>
+          <td>
+            <button>🗑</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
 </main>
